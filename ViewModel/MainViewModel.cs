13a609b517463db0eb59cf47f5cb8206084c3118
230043 Michalina Wysocka <230043@edu.p.lodz.pl>
2023-04-15 02:38:34 +0200

    Implemented more VM methods

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,14 +1,35 @@
-﻿using System.Collections.ObjectModel;
+﻿using Logic;
+using Model;
+using System.Collections.ObjectModel;
 using ViewModel.MVVMBase;
 
 namespace ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
-        // nie widzi API, dk
+        private ModelAbstractAPI _model;
+        private ObservableCollection<IBall> _balls;
         public RelayCommand StartCommand { get; set; }
         public RelayCommand StopCommand { get; set; }
         
+        public MainViewModel() : this(ModelAbstractAPI.CreateAPI()) { }
 
+        public MainViewModel(ModelAbstractAPI model)
+        {
+            // StartCommand = new RelayCommand(...);
+            // StopCommand = new RelayCommand(...);
+            _model = model;
+        }
+
+        public ObservableCollection<IBall> Balls
+        {
+            get => _balls;
+
+            set
+            {
+                _balls = value;
+                RaisePropertyChanged(nameof(Balls));
+            }
+        }
     }
 }
