ef03a721c44df7f77609636a9b9c6738cbfbbee8
230043 Michalina Wysocka <230043@edu.p.lodz.pl>
2023-05-06 22:47:23 +0200

    Added unit and integral tests for Data and Logic

---
+++ DataTest/DataBallTest.cs
@@ -0,0 +1,48 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTest
+{
+    [TestClass]
+    public class DataBallTest
+    {
+        private IDataBall _ball = IDataBall.CreateBall(0, 0, 10, 10, 1, 1);
+
+        [TestMethod]
+        public void TestCreateBall()
+        {
+            Assert.AreEqual(0, _ball.XPosition);
+            Assert.AreEqual(0, _ball.YPosition);
+            Assert.AreEqual(10, _ball.Radius);
+            Assert.AreEqual(10, _ball.Weight);
+        }
+
+        [TestMethod]
+        public void TestMoveBall()
+        {
+            _ball.MoveBall();
+            Assert.AreEqual(1, _ball.XPosition);
+            Assert.AreEqual(1, _ball.YPosition);
+        }
+
+        [TestMethod]
+        public void TestCheckBorderCollision()
+        {
+            var result = _ball.CheckBorderColision(10, 10); 
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestOppositeXYSpeed()
+        {
+            _ball.OppositeXSpeed();
+            Assert.AreEqual(-1, _ball.XSpeed);
+            _ball.OppositeYSpeed();
+            Assert.AreEqual(-1, _ball.YSpeed);
+        }
+    }
+}
---
+++ DataTest/DataTest.cs
@@ -0,0 +1,40 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTest
+{
+    [TestClass]
+    public class DataTest
+    {
+        private DataAbstractAPI _board = DataAbstractAPI.CreateAPI(800, 600, 10);
+
+        [TestMethod]
+        public void TestGetBalls()
+        {
+            _board.CreateBall(0, 0, 10, 10);
+            _board.CreateBall(0, 0, 10, 10);
+            Assert.AreEqual(2, _board.GetBalls().Count);
+        }
+
+        [TestMethod]
+        public void TestClearBoard()
+        {
+            _board.CreateBall(0, 0, 10, 10);
+            _board.CreateBall(0, 0, 10, 10);
+            Assert.AreEqual(2, _board.GetBalls().Count);
+            _board.ClearBoard();
+            Assert.AreEqual(0, _board.GetBalls().Count);
+        }
+
+        [TestMethod]
+        public void TestCreateRandomBallLocation()
+        {
+            _board.CreateRandomBallLocation();
+            Assert.AreEqual(1, _board.GetBalls().Count());  
+        }
+    }
+}
